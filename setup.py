99f875e941437b01466b0aa1551ab9fb337093e2
Kit Lee <wklee4993@gmail.com>
2022-04-12 21:43:46 -0400

    Applied black

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
@@ -12,14 +12,12 @@ setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/wingkitlee/arxiv_collector",
-    packages=find_packages('src'),
-    package_dir={'': 'src'},
+    packages=find_packages("src"),
+    package_dir={"": "src"},
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    entry_points = {
-        'console_scripts': ['arxiv-collect=arxiv_collector.collect:main']
-    }
+    entry_points={"console_scripts": ["arxiv-collect=arxiv_collector.collect:main"]},
 )
